ffe24f644d352d9a05c3c10cc3202db64be5c08d
idayusof <idayusof18@gmail.com>
2021-12-31 18:38:28 +0800

    Update Program.cs
    Calling API for each timestamp

--- NeXTAssessmentTask/Program.cs
+++ NeXTAssessmentTask/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace NeXTAssessmentTask
 {
@@ -20,16 +21,36 @@ namespace NeXTAssessmentTask
                 DateTime univDate = inputDate.ToUniversalTime();
                 univDate = univDate.AddMinutes(-60);
 
-                long[] unixTimestamps = new long[13];
+                //long[] unixTimestamps = new long[13];
                 for (int loop = 0; loop < 13; loop++)
                 {
                     long unixTime = ((DateTimeOffset)univDate).ToUnixTimeSeconds();
 
-                    unixTimestamps[loop] = unixTime;
+                    //unixTimestamps[loop] = unixTime;
                     Console.WriteLine(loop + 1 + ". " + univDate + " (UTC +0) " + unixTime);
                     Console.WriteLine();
 
                     univDate = univDate.AddMinutes(10);
+
+                    #region - calling API based on timestamps
+                    HttpClient client = new HttpClient();
+                    var responseTask = client.GetAsync("https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=" + unixTime + "&units=miles");
+
+                    responseTask.Wait();
+
+                    if (responseTask.IsCompleted)
+                    {
+                        var result = responseTask.Result;
+                        if (result.IsSuccessStatusCode)
+                        {
+                            var messageTask = result.Content.ReadAsStringAsync();
+                            messageTask.Wait();
+
+                            Console.WriteLine(messageTask.Result);
+                            Console.WriteLine();
+                        }
+                    }
+                    #endregion
                 }
             }
             catch (Exception ex)
