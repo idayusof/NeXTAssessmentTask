6d995658db3bc62d58eccfd9c23b317d734bc538
idayusof <idayusof18@gmail.com>
2021-12-31 17:52:11 +0800

    Initial commit

---
+++ NeXTAssessmentTask/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NeXTAssessmentTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("ENTER THE DATE AND TIME (eg: 23/07/2021 6:51pm) : ");
+                string date = Console.ReadLine();
+                Console.WriteLine();
+
+                DateTime inputDate = DateTime.Parse(date);
+                Console.WriteLine(inputDate);
+
+                long unixTime = ((DateTimeOffset)inputDate).ToUnixTimeSeconds();
+
+                Console.WriteLine("UNIXTIMESTAMP : " + unixTime);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
